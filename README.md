b277ce1d4b60a7d1f583f0e189ae986f2fb9481d
ricky1192 <34246173+ricky1192@users.noreply.github.com>
2017-12-04 18:22:17 +0100

    Add files via upload
    Unix_Workbench project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Project's title: Unix Workbench Project
+﻿Project's title: Unix Workbench Project
 lun 4 dic 2017, 18.01.33, CET
-19 
+19
---
+++ guessinggame.sh
@@ -0,0 +1,19 @@
+﻿shopt -s nullglob
+numfiles=(*)
+numfiles=${#numfiles[@]}
+
+echo "How many files are in this folder?"
+read userinput
+
+while [[ "$userinput" != "$numfiles" ]]
+
+do
+        if [[ "$userinput" -gt "$numfiles" ]]
+        then echo "Too high"
+        else echo "Too low"
+        fi
+        echo "How many files are in this folder?"
+        read userinput
+
+done
+echo "Perfect"
